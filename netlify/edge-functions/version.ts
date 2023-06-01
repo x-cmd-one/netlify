36e4876d3720094049ee074f3859f21e518c7e23
Zhengqbbb <1074059947@qq.com>
2023-06-01 19:19:23 +0800

    feat: initial commit

--- netlify/edge-functions/version.ts
+++ netlify/edge-functions/version.ts
@@ -1,5 +1,9 @@
 import type { Context } from "https://edge.netlify.com";
 
 export default async (request: Request, context: Context) => {
-  return Response.json({ version: "v0.1.0" });
+  return Response.json({ 
+    version: "v0.1.0",
+    stage: "alpha",
+    text: "alpha v0.1.0"
+  });
 };
